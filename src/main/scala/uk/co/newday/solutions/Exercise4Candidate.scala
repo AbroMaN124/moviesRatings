b956e70671a09b0540cc58de9eb2d902edbf35bb
Ahmad Furqan <abroman124@Ahmads-Laptop.broadband>
2022-12-16 16:56:26 +0000

    Update output location

--- src/main/scala/uk/co/newday/solutions/Exercise4Candidate.scala
+++ src/main/scala/uk/co/newday/solutions/Exercise4Candidate.scala
@@ -5,6 +5,6 @@ import org.apache.spark.sql.DataFrame
 object Exercise4Candidate {
 
   def execute(movies: DataFrame, ratings: DataFrame, movieRatings:DataFrame, ratingWithRankTop3:DataFrame) = {
-    movieRatings.write.parquet("output/movieRatings.parquet")
+    movieRatings.write.parquet("output/movieRatings")
   }
 }
